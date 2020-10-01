fa9bba8e6a6447d54dc09341e0a7fd22a2598b49
DominicSimon09880 <70915702+DominicSimon09880@users.noreply.github.com>
2020-10-01 02:17:27 -0500

    V1

---
+++ Bible Array/src/BibleArray.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class BibleArray 
+{
+	public static void main(String[] args) throws FileNotFoundException 
+		
+		
+	{
+		Scanner input = new Scanner(new File(System.getProperty("user.dir")+ "/src/books.dat"));
+		while (input.hasNext())
+		{
+		System.out.println(input.nextLine());
+		}
+				input.close();
+	}		
+		
+}
